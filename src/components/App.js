f687186fb106ae175d39d8f662f57c5fe17621a1
Amranabdi1 <amranabdikadir101@gmail.c0m>
2022-10-12 09:34:27 +0300

    completed assignment

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Switch,  Route } from "react-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
